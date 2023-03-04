ca8beb46bd8cbee85e861f5f5f969dcfc3424644
Daniel Jackson <111611125+jackdee0077@users.noreply.github.com>
2023-03-04 12:47:02 -0500

    updates

--- squaring_api/squaring_api/settings.py
+++ squaring_api/squaring_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'corsheaders.middleware.CorsMiddleware',
+]   
 
 ROOT_URLCONF = 'squaring_api.urls'
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
